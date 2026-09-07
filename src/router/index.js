bd5d15b7c9ec02bbae332e48345b76f266bf3306
Elham Mousavi <elham.msv531@gmail.com>
2026-09-07 00:42:12 -0700

    routing

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import page1 from '../views/page1.vue'
+import page2 from '../views/page2.vue'
+import page3 from '../views/page3.vue'
+import page4 from '../views/page4.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'page1',
+            component: page1
+        },
+        {
+            path: '/page2',
+            name: 'page2',
+            component: page2
+        },
+        {
+            path: '/page3',
+            name: 'page3',
+            component: page3
+        },
+        {
+            path: '/page4',
+            name: 'page4',
+            component: page4
+        }
+    ]
+})
+
+export default router
