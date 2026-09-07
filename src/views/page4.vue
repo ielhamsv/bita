cd6b0c9ffdf9a7f6b1735acdefbf3e4655c1e95a
Elham Mousavi <elham.msv531@gmail.com>
2026-09-07 02:34:28 -0700

    gh

--- src/views/page4.vue
+++ src/views/page4.vue
@@ -9,7 +9,7 @@
           <h1>زادروزت مبارک</h1>
           <h3> بابت بزرگترین درسای زندگیم ازت ممنونم و این یدونه تبریکم از قبل بهت بدهکار بودم کم و کاستش رو ببخش</h3>
           <br>
-          <audio controls src="/music/anotherlove.mp3">
+          <audio src="/bita/music/anotherlove.mp3">
             مرورگر شما از پخش صدا پشتیبانی نمی‌کند.
           </audio>
         </div>
